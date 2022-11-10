4c8c9eff338f7a026f51124ed03a56665e3aba16
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-10 19:57:38 +0300

    14

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+﻿// №2 ввести два целых числа и вывести их разность - A-B.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a>> b;
+	int s;
+	s = a - b;
+	cout << s;
+}
+
